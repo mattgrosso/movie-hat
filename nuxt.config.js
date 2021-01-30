374522ce4c8e11a5f89fc41c52edcadf0545733c
Matt Grosso <matt.grosso@framebridge.com>
2021-01-30 17:33:04 -0500

    Adds Bootstrap

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,8 +11,15 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      {
+        rel: 'stylesheet',
+        href:
+          'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css',
+        integrity:
+          'sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1',
+        crossorigin: 'anonymous',
+  },
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
