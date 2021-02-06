0f245aaa8b9df6d14ea530292a3bd95f4732772a
Matt Grosso <matt.grosso@framebridge.com>
2021-02-06 14:06:11 -0500

    Makes app non-scalable for better mobile interation
    I know this is a bad idea for accesibility but there are only two of us who use this site. So if one of us needs better accessibility, I'll come back and fix it.

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,11 @@ export default {
     title: 'Movie Hat',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        name: 'viewport',
+        content:
+          'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
+      },
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [
