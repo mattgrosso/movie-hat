ab06641ae005d1166db220bae1b3d1f3597826bb
Matt Grosso <matt.grosso@framebridge.com>
2024-01-03 12:48:56 -0500

    Resets delete hat title after deletion

--- src/components/HatsList.vue
+++ src/components/HatsList.vue
@@ -238,6 +238,7 @@ export default {
       } else {
         this.getMemberHats();
         this.$refs.closeDeleteHatModal.click();
+        this.deleteHatTitle = null;
       }
     },
     validateEmail (email) {
