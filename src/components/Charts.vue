428779beb82be8b5e50389e3c4e74a85dd2b9a2f
Matt Grosso <matt.grosso@framebridge.com>
2024-07-20 10:32:48 -0400

    Adds chart showing ages of movies in hat

--- src/components/Charts.vue
+++ src/components/Charts.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-    <LineChart class="chart" :chartData="hatCountData" :options="hatCountOptions"/>
+    <!-- <LineChart class="chart" :chartData="hatCountData" :options="hatCountOptions"/> -->
+    <BarChart class="chart" :chartData="hatAdditionAgesData" :options="hatAdditionAgesOptions"/>
   </div>
 </template>
 
 <script>
-import { LineChart } from "vue-chart-3";
+import { LineChart, BarChart } from "vue-chart-3";
 import { Chart, registerables } from "chart.js";
 import randomColor from 'randomcolor';
 
@@ -13,27 +14,87 @@ Chart.register(...registerables);
 
 export default {
   components: {
+    BarChart,
     LineChart
   },
   computed: {
-    addedDates () {
+    inHatNow () {
       if (!this.$store.state.movieHat) {
         return [];
       }
 
-      const inHatNow = this.$store.state.movieHat.map((result) => {
+      return this.$store.state.movieHat.map((result) => {
         return result.timeStamp;
       }).sort((a, b) => a - b);
+    },
+    inHatNowGroupedByMonth () {
+      return this.inHatNow.reduce((acc, timestamp) => {
+        const date = new Date(timestamp);
+        const month = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}`;
 
-      const usedToBeInHat = this.$store.state.history.map((result) => {
+        if (!acc[month]) {
+          acc[month] = 0;
+        }
+
+        acc[month]++;
+
+        return acc;
+      }, {});
+    },
+    inHatNowGroupedByAge () {
+      const now = new Date();
+      return this.inHatNow.reduce((acc, timestamp) => {
+        const date = new Date(timestamp);
+        const diffYears = now.getFullYear() - date.getFullYear();
+        const diffMonths = (now.getMonth() - date.getMonth()) + (12 * diffYears);
+        let category;
+
+        if (diffMonths > 60) { // Older than 5 years
+          category = 'older than 5 years';
+        } else if (diffMonths > 48) { // Older than 4 years
+          category = 'older than 4 years';
+        } else if (diffMonths > 36) { // Older than 3 years
+          category = 'older than 3 years';
+        } else if (diffMonths > 24) { // Older than 3 years
+          category = 'older than 2 years';
+        } else if (diffMonths > 12) { // Older than 1 year
+          category = 'older than 1 year';
+        } else if (diffMonths > 6) { // Older than 6 months
+          category = 'older than 6 months';
+        } else if (diffMonths > 1) { // Older than 1 month
+          category = 'older than 1 month';
+        } else { // Everything else
+          category = 'everything else';
+        }
+
+        if (!acc[category]) {
+          acc[category] = 0;
+        }
+
+        acc[category]++;
+
+        return acc;
+      }, {});
+    },
+    usedToBeInHat () {
+      if (!this.$store.state.history) {
+        return [];
+      }
+
+      return this.$store.state.history.map((result) => {
         if (result.timeStamp) {
           return result.timeStamp;
         } else {
           return 1598932800000;
         }
       }).sort((a, b) => a - b);
+    },
+    allAddedDates () {
+      if (!this.$store.state.movieHat) {
+        return [];
+      }
 
-      return inHatNow.concat(usedToBeInHat);
+      return this.inHatNow.concat(this.usedToBeInHat);
     },
     subtractedDates () {
       if (!this.$store.state.history) {
@@ -84,8 +145,56 @@ export default {
         }
       }
     },
+    hatAdditionAgesData () {
+      const labels = Object.keys(this.inHatNowGroupedByAge).sort((a, b) => new Date(a) - new Date(b));
+      const data = labels.map((month) => {
+        return this.inHatNowGroupedByAge[month];
+      });
+
+      const color = randomColor();
+
+      return {
+        labels: labels,
+        datasets: [
+          {
+            data: data,
+            backgroundColor: color,
+            borderColor: color,
+            tension: 0.5
+          }
+        ]
+      }
+    },
+    hatAdditionAgesOptions () {
+      return {
+        plugins: {
+          legend: {
+            display: false
+          },
+          title: {
+            display: true,
+            text: "Age of Movies Added to Hat",
+          },
+        },
+        backgroundColor: 'rgba(100, 100, 0, 1)',
+        scales: {
+          x: {
+            display: true
+          }
+        },
+        animation: {
+          duration: 2000,
+          easing: 'easeInOutQuad'
+        },
+        elements: {
+          bar: {
+            borderRadius: 3
+          },
+        }
+      }
+    },
     numberOfMoviesAddedByMonth () {
-      return this.addedDates.reduce((acc, timestamp) => {
+      return this.allAddedDates.reduce((acc, timestamp) => {
         const date = new Date(timestamp);
         const month = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}`;
 
